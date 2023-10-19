fadefbf3b2ead29fc1c0b65b3e8f76dd49b287f8
ToniPanovTMS <-l>
2023-10-19 13:54:47 +0300

    GOOD4

--- src/db.java
+++ src/db.java
@@ -3,7 +3,8 @@ public class db{
 public static void main(String[] args)throws ClassNotFoundException, SQLException{
     Connect.Connect();
     Connect.CreateDB();
-    Connect.WriteDB();
+   // Connect.WriteDB();
+    Connect.Write2DB();
     //conn.ReadDB();
     Connect.CloseDB();
 }
